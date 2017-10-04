34349cb6fed6bd5dd823a8104dc78e7eb2840df0
SujitMK <32028914+SujitMK@users.noreply.github.com>
2017-10-04 21:11:25 +0530

    Updated Comments

--- IsAHasARelationJP/src/isAHasARelationPKG/Whatsapp.java
+++ IsAHasARelationJP/src/isAHasARelationPKG/Whatsapp.java
@@ -1,12 +1,12 @@
-package isAHasARelationPKG;
+package isAHasARelationPKG;           // defined package
 
-public class Whatsapp {
+public class Whatsapp {                  // declared and defined a class
 	
 	// Crated the sharing method
-	public void wSharing()
+	public void wSharing()               //declared and defined a method
 	{
-		System.out.println("Video will be shared on Whatsapp.");
-		System.out.println();
+		System.out.println("Video will be shared on Whatsapp.");       // printing info
+		System.out.println();   // printing blank info
 	}
 
 }
