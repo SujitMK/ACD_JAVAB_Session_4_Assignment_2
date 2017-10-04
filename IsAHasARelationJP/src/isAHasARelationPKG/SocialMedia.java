d5650721f68ac07c1e9daec8ec03e3094a64185e
SujitMK <32028914+SujitMK@users.noreply.github.com>
2017-10-04 21:13:51 +0530

    Updated Comments

--- IsAHasARelationJP/src/isAHasARelationPKG/SocialMedia.java
+++ IsAHasARelationJP/src/isAHasARelationPKG/SocialMedia.java
@@ -1,19 +1,19 @@
-package isAHasARelationPKG;
+package isAHasARelationPKG;            // declared package
 
-public class SocialMedia {
+public class SocialMedia {              // declared and defined class
 	
-	// Created two methods one with a welcome note and other with purpose
+	// Created method with a welcome note 
 	public void sMedias() 
 	{
-	   System.out.println();
-	   System.out.println("Social Media consists of Twitter, Facebook, Instagram etc.");
-	   System.out.println();
+	   System.out.println(); // printing blank line
+	   System.out.println("Social Media consists of Twitter, Facebook, Instagram etc.");       // printing info
+	   System.out.println();  // printing blank line
 	}
-	
+	// created a method with purpose
 	public void sPurpose()
 	{
-		System.out.println("Purpose: To provide/display right information and facts.");
-		System.out.println();
+		System.out.println("Purpose: To provide/display right information and facts.");      // printing info
+		System.out.println();   // printing blank line
 	}
 
 }
