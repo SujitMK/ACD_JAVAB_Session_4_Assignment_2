927efc171fb0b7f4bb1849169a0e1ffaa817235c
SujitMK <32028914+SujitMK@users.noreply.github.com>
2017-10-04 21:14:31 +0530

    Updated comments

--- IsAHasARelationJP/src/isAHasARelationPKG/IsAHasARelation.java
+++ IsAHasARelationJP/src/isAHasARelationPKG/IsAHasARelation.java
@@ -1,19 +1,19 @@
-package isAHasARelationPKG;
+package isAHasARelationPKG;      // declared a package
 
-import java.util.Scanner;
+import java.util.Scanner;          // using a scanner utility
 
-public class IsAHasARelation {
+public class IsAHasARelation {        // declared and defined a class
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {          // main method
 
 		// Using Scanner to take dynamic/runtime value for a parameter
 		
 		Scanner input = new Scanner(System.in);
 				
-	    System.out.println();
-	    System.out.println("Please enter 1 to 5 if you want to share the video in Faceboo.");
-		System.out.println("Please enter 6 to 10 if you want to share the video in Whatsapp.");
-		int i = input.nextInt();
+	    System.out.println();     // printing lank info
+	    System.out.println("Please enter 1 to 5 if you want to share the video in Faceboo.");                    // printing info
+		System.out.println("Please enter 6 to 10 if you want to share the video in Whatsapp.");           // printing info
+		int i = input.nextInt();       // taking an user info
 				
 		// created facebook object
 		Facebook f = new Facebook();
@@ -25,7 +25,7 @@ public class IsAHasARelation {
 		// calling Facebook class Method
 		f.fsharing(i);
 		
-		input.close();
+		input.close();         // closing scanner 
 	}
 
 }
